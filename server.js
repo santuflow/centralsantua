c6147a0c98ff4f1217a184329f5c1a1271d06025
santuflow <rodrigosantua2@gmail.com>
2026-02-20 01:34:06 -0300

    proteccion server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname)); 
+app.use(express.static('public'));
 
 // --- BASES DE DATOS EN MEMORIA ---
 let hallazgos = []; 
